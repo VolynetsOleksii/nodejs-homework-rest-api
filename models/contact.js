2d0efa8de586d4f883940b2ac4a4321fd5c35d1f
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-07-29 21:18:03 +0300

    add step1

--- models/contact.js
+++ models/contact.js
@@ -1,26 +1,33 @@
 import { Schema, model } from "mongoose";
 import { handleSaveError, validateAtUpdate } from "./hooks.js";
 
-const contactSchema = new Schema({
-name: {
-    type: String,
-    required: [true, 'Set name for contact'],
-},
-email: {
-    type: String,
-    required: true,
-},
-phone: {
-    type: String,
-    required: true,
-},
-favorite: {
-    type: Boolean,
-    default: false,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
   },
-}, {versionKey: false, timestamps: true});
+  { versionKey: false, timestamps: true }
+);
 
-contactSchema.pre("findOneAndUpdate", validateAtUpdate)
+contactSchema.pre("findOneAndUpdate", validateAtUpdate);
 contactSchema.post("save", handleSaveError);
 contactSchema.post("findOneAndUpdate", handleSaveError);
 
