b1152b57877261db3a83b788f46c564cdfc12621
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-08-05 19:39:11 +0300

    add steps 2, 3

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -5,9 +5,9 @@ import User from "../models/user.js";
 import { ctrlWrapper } from "../decorators/index.js";
 import { HttpError } from "../helpers/index.js";
 import "dotenv/config";
+import gravatar from "gravatar";
 
 const { JWT_SECRET } = process.env;
-
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -17,8 +17,12 @@ const signup = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
@@ -28,7 +32,7 @@ const signup = async (req, res) => {
   });
 };
 const signin = async (req, res) => {
-  const { email, password, subscription="starter" } = req.body;
+  const { email, password, subscription = "starter" } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
     throw HttpError(401, "Email or password is wrong");
@@ -54,31 +58,31 @@ const signin = async (req, res) => {
     },
   });
 };
-const signout = async(req, res)=> {
-    const {_id} = req.user;
-   const idCompare=  await User.findByIdAndUpdate(_id, {token: ""});
-    if (!idCompare) {
-        throw HttpError(401, "Not authorized");
-      }
-    res.status(204).json();
+const signout = async (req, res) => {
+  const { _id } = req.user;
+  const idCompare = await User.findByIdAndUpdate(_id, { token: "" });
+  if (!idCompare) {
+    throw HttpError(401, "Not authorized");
+  }
+  res.status(204).json();
 };
-const getCurrent = (req, res)=> {
-    const { email, subscription} = req.user;
+const getCurrent = (req, res) => {
+  const { email, subscription } = req.user;
 
-    res.json({
-        email,
-        subscription,
-    })
+  res.json({
+    email,
+    subscription,
+  });
 };
 const subscriptionUpdate = async (req, res) => {
-    const { _id, email } = req.user;
-    const { subscription } = req.body;
-    await User.findByIdAndUpdate(_id, { subscription });
-    res.json({
-      email,
-      subscription,
-    });
-  };
+  const { _id, email } = req.user;
+  const { subscription } = req.body;
+  await User.findByIdAndUpdate(_id, { subscription });
+  res.json({
+    email,
+    subscription,
+  });
+};
 
 export default {
   signup: ctrlWrapper(signup),
